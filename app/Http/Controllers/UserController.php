8e96934bc6c6a7ad3de46ca3b54f9399706608b1
Nadiv Nugraha <nadiv.n.ft21@mail.umy.ac.id>
2024-03-19 19:32:36 +0700

    env, mvc, route, relationship

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\clear;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view ('user.index');
+    }
+
+}
