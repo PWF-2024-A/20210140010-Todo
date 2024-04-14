ef7e0b6923e60a862d7bef17ae0ca16f887b882b
Nadiv Nugraha <nadiv.n.ft21@mail.umy.ac.id>
2024-04-14 22:54:35 +0700

    update, delete data

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -15,15 +15,18 @@
                         </div>
                         <div>
                             @if (session('success'))
-                                <p class="text-sm text-green-600 dark:text-green-400">{{ session('success') }}</p>
+                                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
+                                    class="text-sm text-green-600 dark:text-green-400">{{ session('success') }}
+                                </p>
                             @endif
                             @if (session('danger'))
-                                <p class="text-sm text-red-600 dark:text-red-400">{{ session('danger') }}</p>
+                                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
+                                    class="text-sm text-red-600 dark:text-red-400">{{ session('danger') }}
+                                </p>
                             @endif
                         </div>
                     </div>
                 </div>
-
                 <div class="relative overflow-x-auto">
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -43,28 +46,69 @@
                                     <td class="hidden px-6 py-4 md:block">
                                         @if ($todo->is_complete == false)
                                             <span
-                                                class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Ongoing</span>
+                                                class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+                                                Ongoing
+                                            </span>
                                         @else
                                             <span
-                                                class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">Completed</span>
+                                                class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+                                                Completed
+                                            </span>
                                         @endif
                                     </td>
                                     <td class="px-6 py-4">
                                         <div class="flex space-x-3">
-                                            <!-- Action Here -->
+                                            {{-- Action Here --}}
+                                            @if ($todo->is_complete == false)
+                                                <form action="{{ route('todo.complete', $todo) }}" method="Post">
+                                                    @csrf
+                                                    @method('PATCH')
+                                                    <button type="submit" class="text-green-600 dark:text-green-400">
+                                                        Complete
+                                                    </button>
+                                                </form>
+                                            @else
+                                                <form action="{{ route('todo.uncomplete', $todo) }}" method="Post">
+                                                    @csrf
+                                                    @method('PATCH')
+                                                    <button type="submit" class="text-blue-600 dark:text-blue-400">
+                                                        Uncomplete
+                                                    </button>
+                                                </form>
+                                            @endif
+                                            <form action="{{ route('todo.destroy', $todo) }}" method="Post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="text-red-400 dark:text-red-400">
+                                                    Delete
+                                                </button>
+                                            </form>
                                         </div>
                                     </td>
                                 </tr>
                             @empty
                                 <tr class="bg-white dark:bg-gray-800">
-                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">Empty
+                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                        Empty
                                     </td>
                                 </tr>
                             @endforelse
                         </tbody>
                     </table>
                 </div>
+
             </div>
+            @if ($todosCompleted > 1)
+                <div class="p-6 text-xl text-gray-900 dark:text-gray-100">
+                    <form action="{{ route('todo.deleteallcompleted') }}" method="Post">
+                        @csrf
+                        @method('delete')
+                        <x-primary-button>
+                            Delete All Completed Task
+                        </x-primary-button>
+                    </form>
+                </div>
+            @endif
         </div>
     </div>
 </x-app-layout>
---
+++ tugas6.md
@@ -0,0 +1,33 @@
+# Tugas 6
+
+## 1. Todo Completed
+
+![alt text](screenshot/tugas6/completed.png)
+
+## 2. Todo Uncompleted
+
+![alt text](screenshot/tugas6/uncompleted.png)
+
+## 3. Todo Edited
+
+![alt text](screenshot/tugas6/edited.png)
+
+## 4. Todo Deleted
+
+![alt text](screenshot/tugas6/deleted.png)
+
+## 5. Todo Delete All Compteleted Todo
+
+![alt text](<screenshot/tugas6/Delete All.png>)
+
+## 6. User Make Admin
+
+![alt text](<screenshot/tugas6/Make Admin.png>)
+
+## 7. User Remove Admin
+
+![alt text](<screenshot/tugas6/Remove Admin.png>)
+
+## 8. User Delete
+
+![alt text](<screenshot/tugas6/User Delete.png>)
